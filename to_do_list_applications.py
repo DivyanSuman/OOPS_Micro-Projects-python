b1cab03e4f1936cdabff9592881cc3a5fd377850
Divyanshu Prakash <132313184+DivyanSuman@users.noreply.github.com>
2024-04-07 03:06:19 +0530

    Update to_do_list_applications.py

--- to_do_list_applications.py
+++ to_do_list_applications.py
@@ -21,7 +21,7 @@ class To_Do_List:
     def display_tasks (self):
         tasks = []
         if not self.tasks:
-            print('No tasks in To Do List')
+            print('No tasks in To-Do List')
         else:
             for index, value in enumerate(self.tasks, start = 1):
                 task ={
@@ -35,17 +35,20 @@ class To_Do_List:
             print(tasks.to_string(index = False))
 
 
-
+# Examples of adding creating task
 t1 = Task('Learn English', '05-03-2024', 'Low')
 t2 = Task('Learn Python', '20-04-2024', 'High')
 t3 = Task('Larn C', '05-06-2024', 'High')
 t4 = Task('Learn C++', '20-06-2024', 'High')
 
-
+# Examples of uses of To_Do_List
 to_do = To_Do_List()
 to_do.add_tasks(t1)
 to_do.add_tasks(t2)
 to_do.add_tasks(t3)
 to_do.add_tasks(t4)
-
+to_do.del_tasks('Larn C')
 to_do.display_tasks()
+
+
+
